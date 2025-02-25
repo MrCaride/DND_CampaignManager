4c222cfa3e83cd55ce06803da52bfe21515e8007
acpernas <acpernas@esei.uvigo.es>
2025-02-25 17:56:12 +0100

    UX de login actualizada

--- app/auth.py
+++ app/auth.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_user, logout_user, login_required
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 from app.models.user import User
+from app import db
 
 auth = Blueprint('auth', __name__)
 
@@ -17,6 +18,22 @@ def login():
         else:
             flash('Invalid username or password', 'danger')
     return render_template('login.html')
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user:
+            flash('Username already exists', 'danger')
+        else:
+            new_user = User(username=username, password=generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Registration successful! Please log in.', 'success')
+            return redirect(url_for('auth.login'))
+    return render_template('register.html')
 
 @auth.route('/logout')
 @login_required
